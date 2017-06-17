6f1accbdc8156a95e9841f6f0f0c262be9af90cf
yuchong-pan <panyuchong@gmail.com>
2017-06-17 10:31:41 +0800

    Added Vue controller for home

---
+++ static/s/js/home-controller.js
@@ -0,0 +1,13 @@
+var vm = new Vue({
+    el: "#main-container",
+    data: {
+        meetingId: "",
+        userId: "",
+        role: ""
+    },
+    methods: {
+        join: function(meetingId, userId, role) {
+            window.location = "/vox?m=" + meetingId + "&u=" + userId + "&r=" + role;
+        }
+    }
+};
