472d0b4666f154ade5986338cecc104469303cfc
yuchong-pan <panyuchong@gmail.com>
2017-06-18 13:12:45 +0800

    Use fake data

--- static/s/js/controller.js
+++ static/s/js/controller.js
@@ -1,3 +1,59 @@
+var voxFakeData = {
+    "name": 0,
+    "keywords": ["Vox","产品特性"],
+    "start_time": 5,
+    "end_time": 11,
+    "parent": "null",
+    "children": [
+      {
+        "name": 1,
+        "keywords": ["信息的挖掘","音频文件","逻辑性"],
+        "start_time": 11,
+        "end_time": 14,
+        "parent": 0
+      },
+      {
+        "name": 2,
+        "keywords": ["可视化","音频文件"],
+        "start_time": 15,
+        "end_time": 21,
+        "parent": 0
+      },
+      {
+        "name": 3,
+        "keywords": ["思维导图","进度条之间","互动"],
+        "start_time": 22,
+        "end_time": 26,
+        "parent": 0,
+        "children": [
+          {
+            "name": 4,
+            "keywords": ["技术"],
+            "start_time": 27,
+            "end_time": 33,
+            "parent": 3,
+            "children":[
+           {
+             "name": 5,
+             "keywords": ["微软","语音识别","API"],
+             "start_time": 34,
+             "end_time": 40,
+             "parent": 4
+           },
+           {
+            "name": 6,
+             "keywords": ["D3.js","思维导图","可视化","自己设计"],
+             "start_time": 40,
+             "end_time": 48,
+             "parent": 4
+           }
+            ]
+          }
+        ]
+      }
+    ]
+    }
+
 var treeData = [
   {
     "name": 0,
@@ -82,10 +138,12 @@ var vm = new Vue({
             this.recording = !this.recording;
             if (!this.recording) {
                 RecognizerStop(this.SDK, this.recognizer);
+                /*
                 Vue.http.post("/api/realtime", { id: -1 }).then(function(response) {
                     var data = response.body;
                     fakeInit([resonse.body], Math.floor(Date.now() / 1000));
-                });
+                });*/
+                fakeInit([voxFakeData], 50);
             } else {
                 this.setup();
                 RecognizerStart(this.SDK, this.recognizer);
